9eac3b177ade500c13fce965a2e36aec77e96c12
nvtuyen2k <vantuyen.nvt1999@gmail.com>
2021-06-12 16:24:14 +0700

    updaye123123

--- api/User_insert.php
+++ api/User_insert.php
@@ -1,8 +1,19 @@
 <?php
 include ('../services/Users_service.php');
 $service =new UserService();
-$userPhone=$_POST['UserPhone'];
-$userName=$_POST['UserName'];
- echo json_encode($service->add_User($userPhone,$userName));
+$token = $_POST['token'];
+    $phone = $_POST['phone'];
+    $result = $service->loginRegisDevice($phone,$token);
 
+    if($result == 0){
+        $response['error'] = false; 
+        $response['message'] = 'User registered successfully';
+    }elseif($result == 2){
+        $response['error'] = false; 
+        $response['message'] = 'User login successfully';
+    }else{
+        $response['error'] = true;
+        $response['message']='Error, please try again!';
+    }
+    echo json_encode($response);
 ?>
--- services/Users_service.php
+++ services/Users_service.php
@@ -1,3 +1,5 @@
+
+
 <?php
 require_once ('../db/DbConnect.php');
 
@@ -15,14 +17,47 @@ class UserService{
          return $this->db->select('SELECT * FROM users');
         
     }
-    function add_User($userPhone,$userName){
-        return $this->db->query("INSERT INTO users(UserPhone,UserName)  VALUES ('$userPhone','$userName')");
+    function add_User($userPhone,$token){
+        return $this->db->query("INSERT INTO users(UserPhone,Token)  VALUES ('$userPhone','$token')");
     }
 
     function update_User($userPhone,$userMail,$userBirthday,$userImage,$token){
         return $this->db->query("UPDATE users SET UserMail='$userMail',UserBirthday='$userBirthday',UserImage='$userImage',Token='$token' WHERE UserPhone = $userPhone ");
     }
 
+    public function loginRegisDevice($phone,$token){
+        if(!$this->isPhoneExist($phone)){
+            $stmt = $this->conn->prepare("INSERT INTO users (UserPhone, Token,UserImage) VALUES (?,?,'https://assets.webiconspng.com/uploads/2017/09/Avatar-PNG-Image-87443.png')");
+            $stmt->bind_param("ss",$phone,$token);
+            if($stmt->execute())
+                return 0; 
+            return 1; 
+        }else{
+            $stmt = $this->conn->prepare("UPDATE users SET Token = ? WHERE UserPhone = ?");
+            $stmt->bind_param("ss",$token,$phone);
+            if($stmt->execute())
+                return 2; 
+            return 3; 
+        }
+    }
+
+    private function isPhoneExist($phone){
+        $stmt = $this->conn->prepare("SELECT UserID FROM users WHERE UserPhone = ?");
+        $stmt->bind_param("s",$phone);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+    }
+
+    public function getItemUser($phone){
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE UserPhone = ?");
+        $stmt->bind_param("s",$phone);
+        $stmt->execute(); 
+        $result = $stmt->get_result()->fetch_assoc();
+        return $result; 
+    }
 }
 
 ?>
